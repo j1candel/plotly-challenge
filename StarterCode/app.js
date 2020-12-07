0dce48cafab2a3ce6e4aac479ffb105cb5946db7
Jovany Candelario <jovany.candel@gmail.com>
2020-12-06 17:05:35 -0800

    Assigning Ids

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,6 +1,13 @@
 //function threePlots(plotid){
 
-    d3.json("Data/samples.json").then((names)=>{
-        console.log(names)
+    d3.json("Data/samples.json").then((data)=>{
+        console.log(data)
+        
+        let sampleNames = data.samples.filter(name => name.otu_ids.toString() === otu_ids)[0]
+        console.log(sampleNames)
+
     })
+
+
+
 //}
