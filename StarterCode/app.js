58f86db4401893dc63943a7d057ca8bfd0c6d363
Jovany Candelario <jovany.candel@gmail.com>
2020-12-06 18:39:35 -0800

    Bubble chart progress

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,18 +1,59 @@
 function threePlots(id){
 
     d3.json("Data/samples.json").then((data)=>{
-        console.log(data)
+        //console.log(data)
 
         individualData = data.samples[0]
 
-        let ids = individualData.otu_ids.slice(0,10).reverse()
-        console.log(`Top 10 ids: ${ids}`)
+        let allIds= individualData.otu_ids
 
-        let values = individualData.sample_values.reverse()
-        
-        let values10 = values.reverse().slice(0,10)
-        console.log(`Top 10 values: ${values10}`)
+        let ids10 = individualData.otu_ids.slice(0,10).reverse()
+        //console.log(`Top 10 ids: ${ids}`)
 
+        let allLables = individualData.otu_labels
+
+        let labels10 = individualData.otu_labels.slice(0,10).reverse()
+        //console.log(`Top 10 labels:${labels}`)
+
+        let allValues = individualData.sample_values
+
+        let values10 = individualData.sample_values.slice(0,10).reverse()
+        //console.log(`Top 10 values: ${values10}`)
+
+        let otuIds10 = ids10.map(names => "OTU " + names )
+        //console.log(otuIds)
+
+        let trace1={
+            x: values10,
+            y: otuIds10,
+            text: labels10,
+            marker : {
+                color:"blue"
+            },
+            type: "bar",
+            orientation: "h"
+        }
+
+        let barData =[trace1]
+
+        let layout ={
+            title: "Top Ten OTU",
+            yaxis:{tickmode: "linear"},
+            margin:{
+                l:100,
+                r:100,
+                t:100,
+                b:100
+            }
+        }
+
+        Plotly.newPlot("bar", barData, layout)
+
+        let trace2={
+            x:allIds,
+            y:allValues,
+            mode: "markers"
+        }
 
     })
 }
